71ea7e775ec3fb65fa21e6386c4305d3213394f8
suelitonoliveira <sueliton.oliveira@ewave.com.br>
2022-07-26 18:08:50 -0300

    criar cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CriarCursoModule,
+    CursosModule,
+    
+    
   ],
-  providers: [],
+  
+  providers: [LogService],
+ // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
